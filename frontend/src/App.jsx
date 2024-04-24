dda5a113a68e9ee939406b2bb39118dfa0c24383
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-24 01:05:53 -0700

    route to edit by ID

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,19 @@ function App() {
       <BrowserRouter>
         <HeaderComponent />
         <Routes>
+
           {/* Localhost:3000 */}
           <Route path="/" element={<ListEmployeeComponent />}></Route>
+
           {/* Localhost:3000/employees */}
           <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+
           {/* Localhost:3000/add-employee */}
           <Route path="/add-employee" element={<EmployeeComponent />}></Route>
+
+          {/* Localhost:3000/edit-employee{id} */}
+          <Route path="/edit-employee/:id" element={<EmployeeComponent />}></Route>
+
         </Routes>
         {/* <FooterComponent /> */}
       </BrowserRouter>
