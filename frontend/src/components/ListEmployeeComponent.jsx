9e57586e392744a3d69ceabd7a640862a098a67b
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-25 20:12:16 -0700

    adding @Delete button

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { listEmployees } from "../services/EmployeeService";
+import { deleteEmployee, listEmployees } from "../services/EmployeeService";
 import { useNavigate } from "react-router-dom";
 
 const ListEmployeeComponent = () => {
@@ -7,6 +7,10 @@ const ListEmployeeComponent = () => {
   const navigator = useNavigate();
 
   useEffect(() => {
+    getAllEmployees();
+  }, []);
+
+  function getAllEmployees() {
     listEmployees()
       .then((response) => {
         setEmployees(response.data);
@@ -14,7 +18,9 @@ const ListEmployeeComponent = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+
+  }
+
 
   function addEmployee() {
     navigator("/add-employee");
@@ -22,6 +28,16 @@ const ListEmployeeComponent = () => {
 
   function updateEmployee(id) {
     navigator(`/edit-employee/${id}`);
+  }
+
+  function removeEmployee(id) {
+    console.log(id);
+    deleteEmployee(id).then((response) => {
+      console.log(response.data);
+      getAllEmployees();
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   return (
@@ -34,7 +50,7 @@ const ListEmployeeComponent = () => {
         >
           Add Employee
         </button>
-        <table className="table-auto  border-spacing-2  w-full text-left whitespace-no-wrap justify-center self-center items-center"> 
+        <table className="table-auto  border-spacing-2  w-full text-left whitespace-no-wrap justify-center self-center items-center">
           <thead>
             <tr className="bg-gray-100">
               <th className="px-4 py-2 border border-slate-600 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
@@ -49,7 +65,7 @@ const ListEmployeeComponent = () => {
               <th className="px-4 py-2 border border-slate-600 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                 Employee Email Id
               </th>
-              <th className="px-4 py-2 border border-slate-600 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+              <th className=" border border-slate-600 title-font  font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                 Actions
               </th>
 
@@ -73,14 +89,22 @@ const ListEmployeeComponent = () => {
                 <td className="px-4 border border-slate-600 py-3">
                   {employee.email}
                 </td>
-                <td className="px-4 border border-slate-600 py-3">
+                <td className="border border-slate-600 p-1 justify-start">
                   <button
                     type="button"
-                    className="px-3 py-2 border border-transparent text-base font-medium rounded-md text-white 
+                    className="p-2 ml-8 border border-transparent text-base font-medium rounded-md text-white 
                     bg-blue-600 hover:bg-blue-700 "
-                    onClick={()=> updateEmployee(employee.id)}
+                    onClick={() => updateEmployee(employee.id)}
                   >
                     Update
+                  </button>
+                  <button
+                    type="button"
+                    className="p-2 ml-8 border border-transparent text-base font-medium rounded-md text-white 
+                    bg-red-600 hover:bg-red-700 "
+                    onClick={() => removeEmployee(employee.id)}
+                  >
+                    Delete
                   </button>
                 </td>
               </tr>
